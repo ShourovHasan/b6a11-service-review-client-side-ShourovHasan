b9b51b13d5e20c29dfe97aae8d922799f9b7f53d
ShourovHasan <shourovhasan@gmail.com>
2022-11-10 20:33:15 +0600

    Add Service in footer page

--- src/SharedPages/Footer/Footer.js
+++ src/SharedPages/Footer/Footer.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/Logo/fav.webp'
 
 const Footer = () => {
+    const [services, setServices] = useState([]);
+    
+    console.log(services);
+    useEffect(() => {
+        fetch('http://localhost:5000/recentServices')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+            })
+            .catch(error => console.log(error))
+    }, [])
     return (
         <div>
-            <footer className="footer bg-black text-white flex justify-between p-24">
+            <footer className="flex justify-between p-24 text-white bg-black footer">
                 <div>
                     <img src={logo} className="w-20" alt=''></img>
                     <p>Dentistry Services<br />Providing reliable service since 2022</p>
                 </div>
                 <div>
                     <span className="footer-title">Services</span>
-                    <Link to='/' className="link link-hover">Branding</Link>
-                    <Link to='/' className="link link-hover">Design</Link>
-                    <Link to='/' className="link link-hover">Marketing</Link>
-                    <Link to='/' className="link link-hover">Advertisement</Link>
+                    {
+                        services.map(service => <Link to='/services' className="link link-hover">{service.serviceName}</Link>)
+                    }
                 </div>
                 <div>
                     <span className="footer-title">Company</span>
                     <Link to='/' className="link link-hover">About us</Link>
                     <Link to='/' className="link link-hover">Contact</Link>
-                    <Link to='/' className="link link-hover">Jobs</Link>
-                    <Link to='/' className="link link-hover">Press kit</Link>
                 </div>
                 <div>
                     <span className="footer-title">Legal</span>
